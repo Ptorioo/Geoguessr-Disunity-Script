629e33990ee0f8c8d9dc2cdc35bf1770b2867f90
Ptorioo <98724114+Ptorioo@users.noreply.github.com>
2022-06-07 10:44:27 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,155 +1,80 @@
 // ==UserScript==
-// @name		Geoguessr Cheat Update (works on all modes)
+// @name		Geoguessr Disunity Script
 // @namespace		https://github.com/Ptorioo
-// @version		1.0.2
+// @version		1.0.3
 // @description		Press SHIFT + ALT + G and the location of your current geoguessr game will open in a new tab
-// @author		tcortega, Ptorioo
+// @author		Ptorioo
 // @homepage		https://github.com/Ptorioo
-// @downloadURL		https://gist.github.com/Ptorioo/GeoguessrCheat
-// @updateURL		https://gist.github.com/Ptorioo/GeoguessrCheat
-// @match		http*://*/*
+// @match		https://www.geoguessr.com/*
 // @grant		none
-// @run-at		/^(https?)?(\:)?(\/\/)?([^\/]*\.)?geoguessr\.com($|\/.*)/
+// @run-at		document-idle
 // ==/UserScript==
 
-function getCleanUrl() {
-  return window.location.href.replace("/reconnect", "").replace("?client=web", "");
-}
-
-function getJsonData() {
-  const rawData = document.querySelector("pre").innerText;
-  return JSON.parse(rawData);
-}
-
-function apiUrlToGameUrl() {
-  const apiUrl = getCleanUrl();
-  return apiUrl.replace("game-server.", "").replace("/api", "").replace("/v3", "").replace("");
-}
-
-function getGameMode(url) {
-  return url.split(".com/")[1].split("/")[0].toUpperCase();
-}
-
-function getApiUrl(url, gamemode) {
-  url = url.replace("www.", "");
-
-  switch (gamemode) {
-    case "DUELS":
-    case "TEAM-DUELS":
-    case "LIVE-CHALLENGE":
-    case "BULLSEYE":
-    case "BATTLE-ROYALE": {
-      return url.replace("geoguessr.com", "game-server.geoguessr.com/api").replace("team-duels", "duels");
+function getGameMode(url,fetch){
+    if(fetch == 0) {
+        return url.split(".com/")[1].split("/")[0].toUpperCase();
     }
-    case "QUIZ":{
-        return url.replace("geoguessr.com/quiz/play", "game-server.geoguessr.com/api/live-challenge");
+    else {
+        return url.split("api/")[1].split("/")[0].toUpperCase();
     }
-    case "COMPETITIVE-STREAK": {
-      return url.replace("geoguessr.com/competitive-streak", "game-server.geoguessr.com/api/competitive-streaks");
-    }
-    case "GAME": {
-      return url.replace("geoguessr.com/game", "geoguessr.com/api/v3/games");
+}
+
+function getApiUrl(url){
+    url = url.replace("www.", "");
+    switch (getGameMode(url,0)) {
+        case "DUELS":
+        case "TEAM-DUELS":
+        case "LIVE-CHALLENGE":
+        case "BULLSEYE":
+        case "BATTLE-ROYALE": {
+            return url.replace("geoguessr.com", "game-server.geoguessr.com/api").replace("team-duels", "duels");
+        }
+        case "QUIZ":{
+            return url.replace("geoguessr.com/quiz/play", "game-server.geoguessr.com/api/live-challenge");
+        }
+        case "COMPETITIVE-STREAK": {
+            return url.replace("geoguessr.com/competitive-streak", "game-server.geoguessr.com/api/competitive-streaks");
+        }
+        case "GAME": {
+            return url.replace("geoguessr.com/game", "geoguessr.com/api/v3/games");
     }
   }
 }
 
-function isApiPage() {
-  return getCleanUrl().includes("/api/");
-}
-
-function makeApiCall(apiUrl) {
-  window.open(apiUrl, "_blank", "toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=700, height=500, top=0, left=0");
-}
-
-function handleChallenge() {
-  const raw = document.querySelectorAll("#__NEXT_DATA__")[0].text;
-  const json = JSON.parse(raw);
-  const rounds = json.props.pageProps.game.rounds;
-  const { lat, lng } = rounds[rounds.length - 1];
-
-  openMaps({ lat, lng }, "_blank", "popup");
+function makeApiCall(url){
+    fetch(url,{credentials: 'include'}).then(x=>x.json()).then(x=>{
+        let rounds = x.rounds;
+        let cur = x.currentRoundNumber -1;
+        if(Number.isNaN(cur)) cur = x.round -1;
+        let round = rounds[cur];
+        switch (getGameMode(url,1)) {
+            case "DUELS" :
+            case "BULLSEYE" : {
+                round = round.panorama;
+                break;
+            }
+            case "LIVE-CHALLENGE" : {
+                round = round.question.panoramaQuestionPayload.panorama;
+                break;
+            }
+            case "COMPETITIVE-STREAKS" : {
+                round = x.player.currentRound;
+            }
+        }
+        let lat = round.lat;
+        let lng = round.lng;
+        window.open(`https://www.google.com/maps/place/${lat},${lng}/@${lat},${lng-5},6.7z`,"_blank", "toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=800, height=500, top=0, left=0")
+    })
 }
 
 function handleKeyEvent() {
-  const url = getCleanUrl();
-  const gameMode = getGameMode(url);
-
-  if (gameMode == "CHALLENGE") return handleChallenge();
-
-  const apiUrl = getApiUrl(url, gameMode);
-
-  makeApiCall(apiUrl);
-}
-
-function formatDuelsResponse(jsonData) {
-  const rounds = jsonData.rounds.map((r) => ({ lat: r.panorama.lat, lng: r.panorama.lng }));
-  return { round: jsonData.currentRoundNumber, rounds };
-}
-
-function formatStreaksResponse(jsonData) {
-  const rounds = [jsonData.player.currentRound];
-  return { round: 1, rounds };
-}
-
-function formatLiveChallengeResponse(jsonData) {
-  const rounds = jsonData.rounds.map((r) => ({ lat: r.question.panoramaQuestionPayload.panorama.lat, lng: r.question.panoramaQuestionPayload.panorama.lng }));
-  return { round: jsonData.currentRoundNumber, rounds };
-}
-
-function formatBullsEyeResponse(jsonData) {
-  const rounds = jsonData.rounds.map((r) => ({ lat: r.panorama.lat, lng: r.panorama.lng }));
-  return { round: jsonData.currentRoundNumber, rounds };
-}
-
-function formatBattleRoyaleResponse({ currentRoundNumber, rounds }) {
-  return { round: currentRoundNumber, rounds };
-}
-
-function formatGameResponse({ round, rounds }) {
-  return { round, rounds };
-}
-
-function formatResponse(gameMode) {
-  const jsonData = getJsonData();
-
-  if (gameMode == "DUELS"|| gameMode == "TEAM-DUELS") return formatDuelsResponse(jsonData);
-  if (gameMode == "COMPETITIVE-STREAKS") return formatStreaksResponse(jsonData);
-  if (gameMode == "LIVE-CHALLENGE") return formatLiveChallengeResponse(jsonData);
-  if (gameMode == "BULLSEYE") return formatBullsEyeResponse(jsonData);
-  if (gameMode == "BATTLE-ROYALE") return formatBattleRoyaleResponse(jsonData);
-  if (gameMode == "GAMES") return formatGameResponse(jsonData);
-}
-
-function getLocation(gameMode) {
-  const { round, rounds } = formatResponse(gameMode);
-
-  const currentRound = rounds[round - 1];
-
-  return { lat: currentRound.lat, lng: currentRound.lng };
-}
-
-function openMaps(location, target = "_self") {
-  const mapUrl = `https://google.com/maps/place/${location.lat},${location.lng}/@${location.lat},${location.lng-5},6z`;
-  window.open(mapUrl, target);
-}
-
-function handleApiPage() {
-  const gameUrl = apiUrlToGameUrl();
-  const gameMode = getGameMode(gameUrl);
-
-  const location = getLocation(gameMode);
-  openMaps(location);
+    const url = window.location.href.replace("/reconnect", "").replace("?client=web", "");
+    const ApiUrl = getApiUrl(url);
+    makeApiCall(ApiUrl);
 }
 
 (function () {
   "use strict";
-
-  if (!getCleanUrl().includes("geoguessr.com")) return; // Just in case
-
-  if (isApiPage()) {
-    return handleApiPage();
-  }
-
   document.onkeydown = (evt) => {
     evt = evt || window.event;
     if (evt.shiftKey && evt.altKey && evt.keyCode == 71) {
