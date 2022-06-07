f9c317fb30181af00e23fcdc8715ea1f5483bcb2
Ptorioo <98724114+Ptorioo@users.noreply.github.com>
2022-06-07 15:16:08 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 // ==UserScript==
-// @name		Geoguessr Disunity Script
-// @namespace	https://github.com/Ptorioo
-// @version		1.0.3
-// @description	Press SHIFT + ALT + G and the location of your current geoguessr game will open in a new tab
-// @author		Ptorioo
-// @homepage	https://github.com/Ptorioo
-// @match		https://www.geoguessr.com/*
-// @grant		none
-// @run-at		document-idle
+// @name        Geoguessr Disunity Script
+// @namespace   https://github.com/Ptorioo
+// @version     1.0.3
+// @description Press SHIFT + ALT + G and the location of your current geoguessr game will open in a new tab
+// @author      Ptorioo
+// @homepage    https://github.com/Ptorioo
+// @match       https://www.geoguessr.com/*
+// @grant       none
+// @run-at      document-idle
 // @license     MIT
 // ==/UserScript==
 
